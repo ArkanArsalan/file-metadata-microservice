35e7039e1acf6f0e63807d08b58c31e82bb3a036
Arkan Arsalan <arkan.arsalan.amanullah@gmail.com>
2024-05-18 19:26:36 +0700

    feat: add api endpoit for upload image file

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import multer from 'multer';
 
-var app = express();
+dotenv.config();
+
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(path.join(process.cwd(), 'public')));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
 });
 
+const upload = multer({ storage: storage });
 
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'views', 'index.html'));
+});
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  console.log(req.file);
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
 });
